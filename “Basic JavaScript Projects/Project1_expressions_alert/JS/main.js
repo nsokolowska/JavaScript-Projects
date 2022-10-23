af1a2551e28810d5344acf21c50d18c32cc3a20a
nsokolowska <natalia.sokolowska12@gmail.com>
2022-10-23 20:44:58 +0100

    Create main.js

---
+++ “Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+window.alert("WELCOME"); //Welcome alert
+
+// Function 
+var Mathematics = 6, Polish = 4, English = 4, PE = 3, Music = 5, Chemistry = 5;
+var average = ((Mathematics + Polish + English + PE + Music + Chemistry)/6);
+var text = "My average is ";
+var text = text.fontcolor("red");
+document.write(text + average);
+
