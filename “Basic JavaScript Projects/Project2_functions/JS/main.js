c7c56c3c6e551dc7edbe9f964b4ff950b9b62d14
nsokolowska <natalia.sokolowska12@gmail.com>
2022-10-25 21:16:39 +0100

    Update main.js

--- “Basic JavaScript Projects/Project2_functions/JS/main.js
+++ “Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,6 +1,6 @@
-function random(){
-    var lucky = "Your lucky numer is: ";
-    lucky+= (Math.random()*10+1);
-    document.getElementById("number").innerHTML = lucky;
-    document.getElementById("number").style.color = "red";
+function random(){   //function
+    var lucky = "Your lucky numer is: "; //assign variable
+    lucky+= (Math.random()*10+1); //generator random number
+    document.getElementById("number").innerHTML = lucky; 
+    document.getElementById("number").style.color = "red"; //assign color
 }
