07fa9042b9a4b50508ca67422b29ac23b141dcdc
nsokolowska <natalia.sokolowska12@gmail.com>
2022-10-27 20:41:26 +0100

    Update main.js

--- “Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
+++ “Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -7,7 +7,7 @@ function countdown(){
       timer.innerHTML = seconds;
       setTimeout(tick, 1000);
     if (seconds == -1){
-      alert("Time's up")
+      showSlides()
     }
     }
     tick()
