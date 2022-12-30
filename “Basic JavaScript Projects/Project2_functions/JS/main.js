888a665f593fb7e06a0732f1ad38a406e3f331d6
nsokolowska <natalia.sokolowska12@gmail.com>
2022-12-30 20:06:50 +0000

    Delete main.js

--- “Basic JavaScript Projects/Project2_functions/JS/main.js
+++
@@ -1,6 +0,0 @@
-function random(){   //function
-    var lucky = "Your lucky numer is: "; //assign variable
-    lucky+= (Math.random()*10+1); //generator random number
-    document.getElementById("number").innerHTML = lucky; 
-    document.getElementById("number").style.color = "red"; //assign color
-}
